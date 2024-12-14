81e41abea8c2f7b74321c0983ba15d8b83abb1ba
Sergienko Anton <sergienkoanton@mail.ru>
2024-12-14 16:22:35 +0300

    Style

--- src/harrix_swiss_knife/resources_rc.py
+++ src/harrix_swiss_knife/resources_rc.py
@@ -66,10 +66,17 @@ qt_resource_struct = b"\
 \x00\x00\x01\x8d\xfa\xc6\xc7H\
 "
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 qInitResources()
