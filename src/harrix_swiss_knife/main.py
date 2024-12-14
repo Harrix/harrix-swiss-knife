13e5debc329198f275f0bb622e3e1a6d33aebf2a
Sergienko Anton <sergienkoanton@mail.ru>
2024-12-14 16:35:41 +0300

    Add on_rye_new_project_projects and decorator write_in_output_txt

--- src/harrix_swiss_knife/main.py
+++ src/harrix_swiss_knife/main.py
@@ -1,8 +1,63 @@
+import os
 import sys
+import subprocess
+from pathlib import Path
 from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 from PySide6.QtGui import QIcon, QAction
 
 from harrix_swiss_knife import resources_rc
+
+
+def write_in_output_txt(func):
+    output_lines = []
+
+    def wrapper():
+        output_lines.clear()
+        func()
+        file = Path("output.txt")
+        file.write_text("\n".join(output_lines) + "\n", encoding="utf8")
+        print("\n".join(output_lines))
+        os.startfile(file)
+
+    def add_line(line):
+        output_lines.append(line)
+
+    wrapper.add_line = add_line
+    return wrapper
+
+
+@write_in_output_txt
+def on_rye_new_project_projects():
+    f = on_rye_new_project_projects
+    name_project = "test4"
+    path = "C:/Users/sergi/OneDrive/Projects/Python"
+    commands = [
+        f"cd {path}",
+        f"rye init {name_project}",
+        f"code-insiders {path}/{name_project}",
+        f"cd {name_project}",
+        f"rye sync",
+        f'"" | Out-File -FilePath src/{name_project}/main.py -Encoding utf8',
+    ]
+
+    command = ";".join(commands)
+    process = subprocess.run(
+        [
+            "powershell",
+            "-Command",
+            f"[Console]::OutputEncoding = [System.Text.Encoding]::UTF8; {command}",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    output, error = process.stdout, process.stderr
+    if output:
+        f.add_line(output)
+    if error:
+        f.add_line(error)
+
+    f.add_line("end")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -17,14 +72,12 @@ if __name__ == "__main__":
 
     python_menu = QMenu("Python", None)
     menu.addMenu(python_menu)
-
-
-    action_create_rye_projects = QAction("Создать Rye проект в Projects")
-    python_menu.addAction(action_create_rye_projects)
-
-    action_create_rye_github = QAction("Создать Rye проект в Github")
-    python_menu.addAction(action_create_rye_github)
-
+    action_rye_new_project_projects = QAction(
+        "Создать Rye проект в Projects", triggered=on_rye_new_project_projects
+    )
+    python_menu.addAction(action_rye_new_project_projects)
+    action_rye_new_project = QAction("Создать Rye проект в …")
+    python_menu.addAction(action_rye_new_project)
 
     action2 = QAction("Пункт 2")
     exit_action = QAction("Выход", triggered=lambda: QApplication.quit())
