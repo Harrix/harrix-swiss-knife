fc80b24e183dc02e3fcfe317109d6c5a83710c8d
Sergienko Anton <sergienkoanton@mail.ru>
2024-12-15 10:56:35 +0300

    Refactor. Add class MainMenu

--- src/harrix_swiss_knife/main.py
+++ src/harrix_swiss_knife/main.py
@@ -10,32 +10,36 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
+class MainMenu:
+    def __init__(self):
+        self.menu = QMenu()
+
+        self.python_menu = QMenu("Python", None)
+        self.action_rye_new_project_projects = QAction(
+            actions_python.on_rye_new_project_projects.title,
+            triggered=actions_python.on_rye_new_project_projects(),
+        )
+        self.python_menu.addAction(self.action_rye_new_project_projects)
+        self.action_rye_new_project = QAction("Создать Rye проект в …")
+        self.python_menu.addAction(self.action_rye_new_project)
+
+        self.exit_action = QAction("Выход", triggered=lambda: QApplication.quit())
+
+        self.menu.addMenu(self.python_menu)
+        self.menu.addSeparator()
+        self.menu.addAction(self.exit_action)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
     app.setWindowIcon(QIcon(":/assets/logo.svg"))
 
+    main_menu = MainMenu()
+
     tray_icon = QSystemTrayIcon(QIcon(":/assets/logo.svg"), parent=app)
-    menu = QMenu()
-
-    python_menu = QMenu("Python", None)
-    action_rye_new_project_projects = QAction(
-        actions_python.on_rye_new_project_projects.title,
-        triggered=actions_python.on_rye_new_project_projects(),
-    )
-    python_menu.addAction(action_rye_new_project_projects)
-    action_rye_new_project = QAction("Создать Rye проект в …")
-    python_menu.addAction(action_rye_new_project)
-
-    exit_action = QAction("Выход", triggered=lambda: QApplication.quit())
-
-    menu.addMenu(python_menu)
-    menu.addSeparator()
-    menu.addAction(exit_action)
-
-    tray_icon.setContextMenu(menu)
+    tray_icon.setContextMenu(main_menu.menu)
     tray_icon.show()
 
     sys.excepthook = except_hook
-
     sys.exit(app.exec())
