eef2bbd6d49432832fda749ec4ef5238a26e2c70
Sergienko Anton <sergienkoanton@mail.ru>
2024-12-19 03:01:36 +0300

    Style. Move functions on end of file

--- src/harrix_swiss_knife/actions_python.py
+++ src/harrix_swiss_knife/actions_python.py
@@ -4,36 +4,6 @@ from PySide6.QtWidgets import QInputDialog, QFileDialog
 
 from harrix_swiss_knife import functions
 
-
-def find_max_project_number(base_path, start_pattern):
-    pattern = re.compile(start_pattern + r"(\d+)$")
-    max_number = 0
-    for item in os.listdir(base_path):
-        path = os.path.join(base_path, item)
-        if os.path.isdir(path):
-            match = pattern.match(item)
-            if match:
-                number = int(match.group(1))
-                if number > max_number:
-                    max_number = number
-
-    return max_number
-
-
-def create_rye_new_project(name_project, path):
-    commands = f"""
-        cd {path}
-        rye init {name_project}
-        code-insiders {path}/{name_project}
-        cd {name_project}
-        rye sync
-        "" | Out-File -FilePath src/{name_project}/main.py -Encoding utf8
-        Set-Content -Path src/{name_project}/__init__.py -Value $null
-        """
-
-    return functions.run_powershell_script(commands)
-
-
 path_default = "C:/Users/sergi/OneDrive/Projects/Python"
 start_pattern = "python_project_"
 
@@ -80,3 +50,32 @@ class on_rye_new_project_dialog:
 
         result_output = create_rye_new_project(self.name_project, self.path)
         f.add_line(result_output)
+
+
+def find_max_project_number(base_path, start_pattern):
+    pattern = re.compile(start_pattern + r"(\d+)$")
+    max_number = 0
+    for item in os.listdir(base_path):
+        path = os.path.join(base_path, item)
+        if os.path.isdir(path):
+            match = pattern.match(item)
+            if match:
+                number = int(match.group(1))
+                if number > max_number:
+                    max_number = number
+
+    return max_number
+
+
+def create_rye_new_project(name_project, path):
+    commands = f"""
+        cd {path}
+        rye init {name_project}
+        code-insiders {path}/{name_project}
+        cd {name_project}
+        rye sync
+        "" | Out-File -FilePath src/{name_project}/main.py -Encoding utf8
+        Set-Content -Path src/{name_project}/__init__.py -Value $null
+        """
+
+    return functions.run_powershell_script(commands)
