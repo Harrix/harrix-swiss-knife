936f162c0398ed8ea466105e8bd16e56ccef6e74
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-02 07:28:22 +0300

    Refactor. Sort classes, methods, functions

--- src/harrix_swiss_knife/functions.py
+++ src/harrix_swiss_knife/functions.py
@@ -10,48 +10,74 @@ from PySide6.QtGui import QFont, QIcon, QPainter, QPixmap
 from PySide6.QtWidgets import QMenu
 
 
-def write_in_output_txt(is_show_output: bool = True) -> Callable:
+def create_emoji_icon(emoji: str, size: int = 32) -> QIcon:
     """
-    Decorator that captures the output of a function and writes it to a text file.
+    Creates a QIcon object displaying the specified emoji.
 
     Args:
 
-    - `is_show_output` (`bool`, optional): Determines whether to automatically open the output file after writing.
-      Defaults to `True`.
+    - `emoji` (`str`): The emoji character to display.
+    - `size` (`int`, optional): The size of the icon in pixels. Defaults to 32.
 
     Returns:
 
-    - `Callable`: A decorator that wraps the target function with output capturing and writing functionality.
+    - `QIcon`: A QIcon object containing the rendered emoji.
     """
+    pixmap = QPixmap(size, size)
+    pixmap.fill(Qt.transparent)
 
-    def decorator(func: Callable) -> Callable:
-        output_lines = []
+    painter = QPainter(pixmap)
+    font = QFont()
+    font.setPointSize(int(size * 0.8))
+    painter.setFont(font)
+    painter.drawText(pixmap.rect(), Qt.AlignCenter, emoji)
+    painter.end()
 
-        def wrapper(*args, **kwargs):
-            output_lines.clear()
-            start_time = time.time()
-            func(*args, **kwargs)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            wrapper.add_line(f"Execution time: {elapsed_time:.4f} seconds")
-            temp_path = get_project_root() / "temp"
-            if not temp_path.exists():
-                temp_path.mkdir(parents=True, exist_ok=True)
-            file = Path(temp_path / "output.txt")
-            output_text = "\n".join(output_lines) if output_lines else ""
+    return QIcon(pixmap)
 
-            file.write_text(output_text, encoding="utf8")
-            if is_show_output:
-                os.startfile(file)
 
-        def add_line(line: str):
-            output_lines.append(line)
-            print(line)
+def generate_markdown_from_qmenu(menu: QMenu, level: int = 0) -> List[str]:
+    """
+    Recursively traverse the menu and its submenus, generating a Markdown list.
 
-        wrapper.add_line = add_line
-        return wrapper
+    Args:
 
-    return decorator
+    - `menu` (`QMenu`): The menu to traverse.
+    - `level` (`int`, optional): The current indentation level. Defaults to `0`.
+
+    Returns:
+
+    - `List[str]`: A list of strings representing the Markdown lines.
+    """
+    markdown_lines = []
+    for action in menu.actions():
+        if action.menu():  # If the action has a submenu
+            # Add a header for the submenu
+            markdown_lines.append(f'{"  " * level}- **{action.text()}**')
+            # Recursively traverse the submenu
+            markdown_lines.extend(generate_markdown_from_qmenu(action.menu(), level + 1))
+        else:
+            # Add a regular menu item
+            if action.text():
+                markdown_lines.append(f'{"  " * level}- {action.text()}')
+    return markdown_lines
+
+
+def get_project_root() -> Optional[Path]:
+    """
+    Locate the project root directory by searching for the ".venv" folder.
+
+    Args:
+
+    Returns:
+
+    - `Optional[Path]`: The absolute path to the project root directory if found, or `None`.
+    """
+    current_file = Path(__file__).resolve()
+    for parent in current_file.parents:
+        if (parent / ".venv").exists():
+            return parent
+    return current_file.parent
 
 
 def run_powershell_script(commands: str) -> str:
@@ -158,71 +184,45 @@ def run_powershell_script_as_admin(commands: str) -> str:
     return "\n".join(filter(None, res_output))
 
 
-def get_project_root() -> Optional[Path]:
+def write_in_output_txt(is_show_output: bool = True) -> Callable:
     """
-    Locate the project root directory by searching for the ".venv" folder.
+    Decorator that captures the output of a function and writes it to a text file.
 
     Args:
 
-    Returns:
-
-    - `Optional[Path]`: The absolute path to the project root directory if found, or `None`.
-    """
-    current_file = Path(__file__).resolve()
-    for parent in current_file.parents:
-        if (parent / ".venv").exists():
-            return parent
-    return current_file.parent
-
-
-def create_emoji_icon(emoji: str, size: int = 32) -> QIcon:
-    """
-    Creates a QIcon object displaying the specified emoji.
-
-    Args:
-
-    - `emoji` (`str`): The emoji character to display.
-    - `size` (`int`, optional): The size of the icon in pixels. Defaults to 32.
+    - `is_show_output` (`bool`, optional): Determines whether to automatically open the output file after writing.
+      Defaults to `True`.
 
     Returns:
 
-    - `QIcon`: A QIcon object containing the rendered emoji.
+    - `Callable`: A decorator that wraps the target function with output capturing and writing functionality.
     """
-    pixmap = QPixmap(size, size)
-    pixmap.fill(Qt.transparent)
 
-    painter = QPainter(pixmap)
-    font = QFont()
-    font.setPointSize(int(size * 0.8))
-    painter.setFont(font)
-    painter.drawText(pixmap.rect(), Qt.AlignCenter, emoji)
-    painter.end()
+    def decorator(func: Callable) -> Callable:
+        output_lines = []
 
-    return QIcon(pixmap)
+        def wrapper(*args, **kwargs):
+            output_lines.clear()
+            start_time = time.time()
+            func(*args, **kwargs)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            wrapper.add_line(f"Execution time: {elapsed_time:.4f} seconds")
+            temp_path = get_project_root() / "temp"
+            if not temp_path.exists():
+                temp_path.mkdir(parents=True, exist_ok=True)
+            file = Path(temp_path / "output.txt")
+            output_text = "\n".join(output_lines) if output_lines else ""
 
+            file.write_text(output_text, encoding="utf8")
+            if is_show_output:
+                os.startfile(file)
 
-def generate_markdown_from_qmenu(menu: QMenu, level: int = 0) -> List[str]:
-    """
-    Recursively traverse the menu and its submenus, generating a Markdown list.
+        def add_line(line: str):
+            output_lines.append(line)
+            print(line)
 
-    Args:
+        wrapper.add_line = add_line
+        return wrapper
 
-    - `menu` (`QMenu`): The menu to traverse.
-    - `level` (`int`, optional): The current indentation level. Defaults to `0`.
-
-    Returns:
-
-    - `List[str]`: A list of strings representing the Markdown lines.
-    """
-    markdown_lines = []
-    for action in menu.actions():
-        if action.menu():  # If the action has a submenu
-            # Add a header for the submenu
-            markdown_lines.append(f'{"  " * level}- **{action.text()}**')
-            # Recursively traverse the submenu
-            markdown_lines.extend(generate_markdown_from_qmenu(action.menu(), level + 1))
-        else:
-            # Add a regular menu item
-            if action.text():
-                markdown_lines.append(f'{"  " * level}- {action.text()}')
-    return markdown_lines
+    return decorator
