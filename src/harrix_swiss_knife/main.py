e3f376cc3de52782c265d66b420f8ee9600d0d6d
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-02 07:20:20 +0300

    Style. Correction code

--- src/harrix_swiss_knife/main.py
+++ src/harrix_swiss_knife/main.py
@@ -4,8 +4,7 @@ from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import QApplication, QMenu, QSystemTrayIcon
 
 from harrix_swiss_knife import resources_rc  # noqa
-from harrix_swiss_knife import (actions_images, actions_notes, actions_py,
-                                actions_windows)
+from harrix_swiss_knife import actions_images, actions_notes, actions_py, actions_windows
 from harrix_swiss_knife import functions as f
 
 
