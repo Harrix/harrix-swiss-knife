4d6aa0cb1f63ae5bf2092f2cd1fd48e13244a70a
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-12 08:55:59 +0300

    Refactor. Use class ActionBase in actions_py.py

--- src/harrix_swiss_knife/actions_py.py
+++ src/harrix_swiss_knife/actions_py.py
@@ -1,130 +1,88 @@
 from pathlib import Path
 
 import harrix_pylib as h
-from PySide6.QtWidgets import QFileDialog, QInputDialog
+
+from harrix_swiss_knife import action_base
 
 config = h.dev.load_config("config/config.json")
 
 
-class on_py_sort_code:
+class on_py_sort_code(action_base.ActionBase):
     icon: str = "📶"
     title: str = "Sort classes, methods, functions in one PY file"
 
-    def __init__(self, **kwargs): ...
-
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
-        filename, _ = QFileDialog.getOpenFileName(
-            None,
-            "Select an Python File",
-            config["path_github"],
-            "Image Files (*.py);;All Files (*)",
+    def execute(self, *args, **kwargs):
+        filename = self.get_open_filename(
+            "Select an Python File", config["path_github"], "Image Files (*.py);;All Files (*)"
         )
-
         if not filename:
-            self.__call__.add_line("❌ The file was not selected.")
             return
 
         try:
             h.dev.sort_py_code(filename)
-            self.__call__.add_line(f"File {filename} is applied.")
+            self.add_line(f"File {filename} is applied.")
         except Exception:
-            self.__call__.add_line(f"❌ File {filename} is not applied.")
+            self.add_line(f"❌ File {filename} is not applied.")
 
 
-class on_py_sort_code_folder:
+class on_py_sort_code_folder(action_base.ActionBase):
     icon: str = "📶"
     title: str = "Sort classes, methods, functions in PY files"
 
-    def __init__(self, **kwargs): ...
-
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
-        title = "Project folder"
-        folder_path = QFileDialog.getExistingDirectory(None, title, config["path_github"])
-
-        if folder_path:
-            self.path: str = folder_path
-        else:
-            self.__call__.add_line("❌ The folder was not selected.")
+    def execute(self, *args, **kwargs):
+        folder_path = self.get_existing_directory("Select a Project folder", config["path_github"])
+        if not folder_path:
             return
 
-        self.__call__.add_line(h.file.apply_func(folder_path, ".py", h.dev.sort_py_code))
+        try:
+            self.add_line(h.file.apply_func(folder_path, ".py", h.dev.sort_py_code))
+        except Exception as e:
+            self.add_line(f"❌ Error: {e}")
 
 
-class on_py_sort_isort_fmt_python_code_folder:
+class on_py_sort_isort_fmt_python_code_folder(action_base.ActionBase):
     icon: str = "🌟"
     title: str = "isort, ruff format, sort in PY files"
 
-    def __init__(self, **kwargs): ...
-
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
-        title = "Project folder"
-        folder_path = QFileDialog.getExistingDirectory(None, title, config["path_github"])
-
-        if not (folder_path):
-            self.__call__.add_line("❌ The folder was not selected.")
+    def execute(self, *args, **kwargs):
+        folder_path = self.get_existing_directory("Select a Project folder", config["path_github"])
+        if not folder_path:
             return
 
-        commands = f"""
-            cd {folder_path}
-            isort .
-            ruff format
-            """
-
-        self.__call__.add_line(h.dev.run_powershell_script(commands))
-        self.__call__.add_line(h.file.apply_func(folder_path, ".py", h.dev.sort_py_code))
+        commands = f"cd {folder_path}\nisort .\nruff format"
+        self.add_line(h.dev.run_powershell_script(commands))
+        self.add_line(h.file.apply_func(folder_path, ".py", h.dev.sort_py_code))
 
 
-class on_py_uv_new_project:
+class on_py_uv_new_project(action_base.ActionBase):
     icon: str = "uv.svg"
     title: str = "New uv project in Projects"
 
-    def __init__(self, **kwargs): ...
+    def execute(self, *args, **kwargs):
+        path = config["path_py_projects"]
+        max_project_number = h.file.find_max_folder_number(path, config["start_pattern_py_projects"])
+        name_project: str = f"python_project_{f'{(max_project_number + 1):02}'}"
 
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
-        self.path: str = config["path_py_projects"]
-        max_project_number = h.file.find_max_folder_number(self.path, config["start_pattern_py_projects"])
-        self.name_project: str = f"python_project_{f'{(max_project_number + 1):02}'}"
-
-        self.__call__.add_line(py_create_uv_new_project(self.name_project, self.path))
+        self.add_line(py_create_uv_new_project(name_project, path))
 
 
-class on_py_uv_new_project_dialog:
+class on_py_uv_new_project_dialog(action_base.ActionBase):
     icon: str = "uv.svg"
     title: str = "New uv project in …"
 
-    def __init__(self, **kwargs): ...
-
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
-        title: str = "Project name"
-        label: str = "Enter the name of the project (English, without spaces):"
-        project_name, ok = QInputDialog.getText(None, title, label)
-
-        if ok and project_name:
-            self.name_project: str = project_name
-        else:
-            self.__call__.add_line("❌ The name of the project was not entered.")
+    def execute(self, *args, **kwargs):
+        project_name = self.get_text_input("Project name", "Enter the name of the project (English, without spaces):")
+        if not project_name:
             return
 
-        self.name_project = self.name_project.replace(" ", "-")
-
-        title = "Project folder"
-        folder_path = QFileDialog.getExistingDirectory(None, title, config["path_py_projects"])
-
-        if folder_path:
-            self.path: str = folder_path
-        else:
-            self.__call__.add_line("❌ The folder was not selected.")
+        folder_path = self.get_existing_directory("Select a Project folder", config["path_py_projects"])
+        if not folder_path:
             return
 
-        self.__call__.add_line(py_create_uv_new_project(self.name_project, self.path))
+        self.add_line(py_create_uv_new_project(project_name.replace(" ", "-"), folder_path))
 
 
-def py_create_uv_new_project(name_project: str, path: str | Path) -> str:
+def py_create_uv_new_project(project_name: str, path: str | Path) -> str:
     """
     Creates a new project using uv, initializes it, and sets up necessary files.
 
@@ -139,25 +97,25 @@ def py_create_uv_new_project(name_project: str, path: str | Path) -> str:
     """
     commands = f"""
         cd {path}
-        uv init --package {name_project}
-        cd {name_project}
+        uv init --package {project_name}
+        cd {project_name}
         uv sync
         uv add --dev isort
         uv add --dev ruff
         uv add --dev pytest
-        New-Item -ItemType File -Path src/{name_project}/main.py -Force
-        New-Item -ItemType File -Path src/{name_project}/__init__.py -Force
+        New-Item -ItemType File -Path src/{project_name}/main.py -Force
+        New-Item -ItemType File -Path src/{project_name}/__init__.py -Force
         Add-Content -Path pyproject.toml -Value "`n[tool.ruff]"
         Add-Content -Path pyproject.toml -Value "line-length = 120"
-        {config["editor"]} {path}/{name_project}
+        {config["editor"]} {path}/{project_name}
         """
 
     res = h.dev.run_powershell_script(commands)
 
-    readme_path = Path(path) / name_project / "README.md"
+    readme_path = Path(path) / project_name / "README.md"
     try:
         with readme_path.open("a", encoding="utf-8") as file:
-            file.write(f"# {name_project}\n\n{config['cli_commands']}")
+            file.write(f"# {project_name}\n\n{config['cli_commands']}")
         res += f"Content successfully added to {readme_path}"
     except FileNotFoundError:
         res += f"File not found: {readme_path}"
