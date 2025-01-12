a94ccc682e67fa897b6720bb1dec63f26609428a
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-13 00:57:16 +0300

    Add action ""Get a list of movies, books for web""

--- src/harrix_swiss_knife/main.py
+++ src/harrix_swiss_knife/main.py
@@ -40,6 +40,7 @@ class MainMenu(main_menu_base.MainMenuBase):
         self.add_item(self.menu_md, actions_md.on_markdown_add_author_book)
         self.add_item(self.menu_md, actions_md.on_markdown_add_image_captions)
         self.add_item(self.menu_md, actions_md.on_markdown_add_image_captions_folder)
+        self.add_item(self.menu_md, actions_md.on_markdown_get_list_movies_books)
 
         # Menu File operations
         self.menu_file = self.new_menu("File operations", "🪟")
