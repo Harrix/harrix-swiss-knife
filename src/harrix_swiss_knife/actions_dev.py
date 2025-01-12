8940e4ae355ebeb63dca1b636c4f68c84dcf96e2
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-12 09:07:14 +0300

    Refactor. Use class ActionBase in actions_dev.py

--- src/harrix_swiss_knife/actions_dev.py
+++ src/harrix_swiss_knife/actions_dev.py
@@ -1,43 +1,40 @@
 import harrix_pylib as h
 from PySide6.QtWidgets import QApplication
 
+from harrix_swiss_knife import action_base
+
 config = h.dev.load_config("config/config.json")
 
 
-class on_dev_exit:
+class on_dev_exit(action_base.ActionBase):
     icon: str = "×"
     title: str = "Exit"
 
     def __init__(self, **kwargs):
         self.parent = kwargs.get("parent", None)
 
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
+    def execute(self, *args, **kwargs):
         if self.parent:
             QApplication.quit()
 
 
-class on_dev_get_menu:
+class on_dev_get_menu(action_base.ActionBase):
     icon: str = "☰"
     title: str = "Get the list of items from this menu"
 
     def __init__(self, **kwargs):
         self.parent = kwargs.get("parent", None)
 
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
+    def execute(self, *args, **kwargs):
         if self.parent:
-            self.__call__.add_line(self.parent.get_menu())
+            self.add_line(self.parent.get_menu())
 
 
-class on_dev_open_config_json:
+class on_dev_open_config_json(action_base.ActionBase):
     icon: str = "⚙️"
     title: str = "Open config.json"
 
-    def __init__(self, **kwargs): ...
-
-    @h.dev.write_in_output_txt(is_show_output=False)
-    def __call__(self, *args, **kwargs) -> None:
-        commands = f"{config['editor']} {h.dev.get_project_root() / 'config.json'}"
+    def execute(self, *args, **kwargs):
+        commands = f"{config['editor']} {h.dev.get_project_root() /'config' / 'config.json'}"
         output = h.dev.run_powershell_script(commands)
-        self.__call__.add_line(output)
+        self.add_line(output)
