e813ed3dc7f0082cbe1ff732d006dbb3917587e6
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-19 00:13:37 +0300

    Refactor. Use import harrix_swiss_knife as hsk

---
+++ src/harrix_swiss_knife/__init__.py
@@ -0,0 +1,5 @@
+from harrix_swiss_knife import actions_dev as dev  # noqa
+from harrix_swiss_knife import actions_file as file  # noqa
+from harrix_swiss_knife import actions_images as images  # noqa
+from harrix_swiss_knife import actions_md as md  # noqa
+from harrix_swiss_knife import actions_py as py  # noqa
