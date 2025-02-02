6ec6eaf76603e67851253ca8618b84f4269a5dd7
Sergienko Anton <sergienkoanton@mail.ru>
2025-02-02 21:57:30 +0300

    ➕ Add action "Fitness tracker"

--- src/harrix_swiss_knife/__init__.py
+++ src/harrix_swiss_knife/__init__.py
@@ -1,6 +1,6 @@
+from harrix_swiss_knife import actions_apps as apps  # noqa
 from harrix_swiss_knife import actions_dev as dev  # noqa
 from harrix_swiss_knife import actions_file as file  # noqa
 from harrix_swiss_knife import actions_images as images  # noqa
 from harrix_swiss_knife import actions_md as md  # noqa
 from harrix_swiss_knife import actions_py as py  # noqa
-from harrix_swiss_knife import sport_window as sport_window  # noqa
---
+++ src/harrix_swiss_knife/actions_apps.py
@@ -0,0 +1,21 @@
+import harrix_pylib as h
+
+from harrix_swiss_knife import action_base, fitness
+
+config = h.dev.load_config("config/config.json")
+
+
+class on_fitness(action_base.ActionBase):
+    icon: str = "🏃🏻"
+    title: str = "Fitness tracker"
+
+    def __init__(self, **kwargs):
+        self.parent = kwargs.get("parent", None)
+        self.main_window = None
+
+    def execute(self, *args, **kwargs):
+        if self.main_window is None:
+            self.main_window = fitness.MainWindow()
+        self.main_window.show()
+        self.main_window.raise_()
+        self.main_window.activateWindow()
