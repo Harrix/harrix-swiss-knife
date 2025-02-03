c54dc6766c95625a3ad5b145890cb8c0b3f39b95
Sergienko Anton <sergienkoanton@mail.ru>
2025-02-04 01:09:55 +0300

    🗑️ Delete SVG files

--- src/harrix_swiss_knife/resources_rc.py
+++ src/harrix_swiss_knife/resources_rc.py
@@ -1,6 +1,6 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 6.8.1
+# Created by: The Resource Compiler for Qt version 6.8.2
 # WARNING! All changes made in this file will be lost!
 
 from PySide6 import QtCore
@@ -44,118 +44,6 @@ class=\x22st0\x22 d=\x22M\
 .9 15.7 273.6-30\
 .6 154 21.2z\x22/><\
 /svg>\
-\x00\x00\x05\x98\
-<\
-svg xmlns=\x22http:\
-//www.w3.org/200\
-0/svg\x22 xml:space\
-=\x22preserve\x22 view\
-Box=\x220 0 250 250\
-\x22><linearGradien\
-t id=\x22a\x22 x1=\x22374\
-.152\x22 x2=\x22616.43\
-7\x22 y1=\x222.887\x22 y2\
-=\x22-205.515\x22 grad\
-ientTransform=\x22m\
-atrix(.5625 0 0 \
--.568 -215.025 8\
-.76)\x22 gradientUn\
-its=\x22userSpaceOn\
-Use\x22><stop offse\
-t=\x220\x22 style=\x22sto\
-p-color:#5a9fd4\x22\
-/><stop offset=\x22\
-1\x22 style=\x22stop-c\
-olor:#306998\x22/><\
-/linearGradient>\
-<path d=\x22M123.5 \
-0C113.3 0 103.6.\
-9 95 2.4c-25.2 4\
-.5-29.8 13.8-29.\
-8 31v22.7h59.6v7\
-.6H42.9c-17.3 0-\
-32.5 10.4-37.2 3\
-0.2-5.5 22.7-5.7\
- 36.9 0 60.5C9.9\
- 172 20 184.6 37\
-.4 184.6h20.5v-2\
-7.2c0-19.7 17-37\
- 37.2-37h59.5c16\
-.6 0 29.8-13.6 2\
-9.8-30.3V33.4c0-\
-16.1-13.6-28.3-2\
-9.8-31C144.3.7 1\
-33.7 0 123.5 0M9\
-1.3 18.3c6.2 0 1\
-1.2 5.1 11.2 11.\
-4S97.5 41 91.3 4\
-1s-11.2-5.1-11.2\
--11.3c0-6.3 5-11\
-.4 11.2-11.4\x22 st\
-yle=\x22fill:url(#a\
-)\x22/><linearGradi\
-ent id=\x22b\x22 x1=\x227\
-10.159\x22 x2=\x22623.\
-51\x22 y1=\x22-331.637\
-\x22 y2=\x22-209.217\x22 \
-gradientTransfor\
-m=\x22matrix(.5625 \
-0 0 -.568 -215.0\
-25 8.76)\x22 gradie\
-ntUnits=\x22userSpa\
-ceOnUse\x22><stop o\
-ffset=\x220\x22 style=\
-\x22stop-color:#ffd\
-43b\x22/><stop offs\
-et=\x221\x22 style=\x22st\
-op-color:#ffe873\
-\x22/></linearGradi\
-ent><path d=\x22M19\
-1.8 63.7v26.5c0 \
-20.5-17.4 37.8-3\
-7.2 37.8H95.1c-1\
-6.3 0-29.8 13.9-\
-29.8 30.3V215c0 \
-16.1 14 25.6 29.\
-8 30.3 18.9 5.5 \
-36.9 6.5 59.5 0 \
-15-4.3 29.8-13.1\
- 29.8-30.3v-22.7\
-h-59.5v-7.6h89.3\
-c17.3 0 23.8-12.\
-1 29.8-30.2 6.2-\
-18.7 6-36.6 0-60\
-.5-4.3-17.2-12.5\
--30.2-29.8-30.2z\
-m-33.5 143.6c6.2\
- 0 11.2 5.1 11.2\
- 11.3 0 6.3-5 11\
-.4-11.2 11.4s-11\
-.2-5.1-11.2-11.4\
-c0-6.2 5.1-11.3 \
-11.2-11.3\x22 style\
-=\x22fill:url(#b)\x22/\
-></svg>\
-\x00\x00\x01\x17\
-<\
-svg xmlns=\x22http:\
-//www.w3.org/200\
-0/svg\x22 xml:space\
-=\x22preserve\x22 view\
-Box=\x220 0 250 250\
-\x22><path d=\x22m0 1.\
-1.5 124.5.4 99.6\
-c.1 13.7 11.3 24\
-.8 25 24.8l99.6-\
-.4 62.2-.3h6.3c1\
-3.7-.1 24.8-11.4\
- 24.8-25.1h11.4V\
-249h19.7l-1-249-\
-114.5.5.5 99.2v6\
-2.1h-20.4l.5-62-\
-.5-99.2z\x22 style=\
-\x22fill:#de5fe9\x22/>\
-</svg>\
 "
 
 qt_resource_name = b"\
@@ -167,36 +55,21 @@ qt_resource_name = b"\
 \x05\xe2T\xa7\
 \x00l\
 \x00o\x00g\x00o\x00.\x00s\x00v\x00g\
-\x00\x06\
-\x07|Z\xc7\
-\x00p\
-\x00y\x00.\x00s\x00v\x00g\
-\x00\x06\
-\x07\xc9Z\xc7\
-\x00u\
-\x00v\x00.\x00s\x00v\x00g\
 "
 
 qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x12\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x94G\x05W\x80\
-\x00\x00\x00(\x00\x00\x00\x00\x00\x01\x00\x00\x02:\
-\x00\x00\x01\x94G\x05W\x80\
-\x00\x00\x00:\x00\x00\x00\x00\x00\x01\x00\x00\x07\xd6\
-\x00\x00\x01\x94G[\xeb\xdd\
+\x00\x00\x01\x94z\xbfm\x0f\
 "
-
-
-def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
-
 
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
 qInitResources()
