9201d2171f73ec125c02a396a425b3b4f6663ee3
Sergienko Anton <sergienkoanton@mail.ru>
2025-02-09 10:13:56 +0300

    ⬆️ Update harrix-pylib

--- src/harrix_swiss_knife/actions_md.py
+++ src/harrix_swiss_knife/actions_md.py
@@ -2,58 +2,12 @@ from datetime import datetime
 from pathlib import Path
 
 import harrix_pylib as h
+from PySide6.QtGui import QClipboard
+from PySide6.QtWidgets import QApplication, QMessageBox
 
 from harrix_swiss_knife import action_base
 
 config = h.dev.load_config("config/config.json")
-
-
-class on_generate_author_book(action_base.ActionBase):
-    icon: str = "❞"
-    title: str = "Quotes. Add author and title"
-    is_show_output = True
-
-    def execute(self, *args, **kwargs):
-        folder_path = self.get_existing_directory("Select a folder with quotes", config["path_quotes"])
-        if not folder_path:
-            return
-
-        try:
-            self.add_line(h.file.apply_func(folder_path, ".md", h.md.generate_author_book))
-        except Exception as e:
-            self.add_line(f"❌ Ошибка: {e}")
-
-
-class on_generate_image_captions(action_base.ActionBase):
-    icon: str = "🌄"
-    title: str = "Add image captions in one MD"
-
-    def execute(self, *args, **kwargs):
-        filename = self.get_open_filename(
-            "Open Markdown file", config["path_articles"], "Markdown (*.md);;All Files (*)"
-        )
-        if not filename:
-            return
-
-        try:
-            self.add_line(h.md.generate_image_captions(filename))
-        except Exception as e:
-            self.add_line(f"❌ Ошибка: {e}")
-
-
-class on_generate_image_captions_folder(action_base.ActionBase):
-    icon: str = "🌄"
-    title = "Add image captions in …"
-
-    def execute(self, *args, **kwargs):
-        folder_path = self.get_existing_directory("Select a folder with Markdown files", config["path_articles"])
-        if not folder_path:
-            return
-
-        try:
-            self.add_line(h.file.apply_func(folder_path, ".md", h.md.generate_image_captions))
-        except Exception as e:
-            self.add_line(f"❌ Error: {e}")
 
 
 class on_diary_new(action_base.ActionBase):
@@ -100,6 +54,54 @@ class on_format_yaml(action_base.ActionBase):
 
         try:
             self.add_line(h.file.apply_func(folder_path, ".md", h.md.format_yaml))
+        except Exception as e:
+            self.add_line(f"❌ Error: {e}")
+
+
+class on_generate_author_book(action_base.ActionBase):
+    icon: str = "❞"
+    title: str = "Quotes. Add author and title"
+    is_show_output = True
+
+    def execute(self, *args, **kwargs):
+        folder_path = self.get_existing_directory("Select a folder with quotes", config["path_quotes"])
+        if not folder_path:
+            return
+
+        try:
+            self.add_line(h.file.apply_func(folder_path, ".md", h.md.generate_author_book))
+        except Exception as e:
+            self.add_line(f"❌ Ошибка: {e}")
+
+
+class on_generate_image_captions(action_base.ActionBase):
+    icon: str = "🌄"
+    title: str = "Add image captions in one MD"
+
+    def execute(self, *args, **kwargs):
+        filename = self.get_open_filename(
+            "Open Markdown file", config["path_articles"], "Markdown (*.md);;All Files (*)"
+        )
+        if not filename:
+            return
+
+        try:
+            self.add_line(h.md.generate_image_captions(filename))
+        except Exception as e:
+            self.add_line(f"❌ Ошибка: {e}")
+
+
+class on_generate_image_captions_folder(action_base.ActionBase):
+    icon: str = "🌄"
+    title = "Add image captions in …"
+
+    def execute(self, *args, **kwargs):
+        folder_path = self.get_existing_directory("Select a folder with Markdown files", config["path_articles"])
+        if not folder_path:
+            return
+
+        try:
+            self.add_line(h.file.apply_func(folder_path, ".md", h.md.generate_image_captions))
         except Exception as e:
             self.add_line(f"❌ Error: {e}")
 
@@ -151,6 +153,19 @@ class on_get_list_movies_books(action_base.ActionBase):
                 count += 1
 
         self.add_line(f"\nCount: {count}")
+
+
+class on_increase_heading_level_content(action_base.ActionBase):
+    icon: str = "👉"
+    title = "Increase heading level"
+
+    def execute(self, *args, **kwargs):
+        content = self.get_text_textarea("Markdown content", "Input Markdown content")
+        new_content = h.md.increase_heading_level_content(content)
+        self.add_line(new_content)
+        clipboard = QApplication.clipboard()
+        clipboard.setText(new_content, QClipboard.Clipboard)
+        QMessageBox.information(None, "Copy", "Text copied to clipboard!")
 
 
 class on_new_article(action_base.ActionBase):
@@ -248,58 +263,3 @@ class on_sort_sections_folder(action_base.ActionBase):
             self.add_line(h.file.apply_func(folder_path, ".md", h.md.generate_image_captions))
         except Exception as e:
             self.add_line(f"❌ Error: {e}")
-
-
-from PySide6.QtWidgets import QApplication, QMessageBox
-from PySide6.QtGui import QClipboard
-
-class on_increase_heading_level_content(action_base.ActionBase):
-    icon: str = "👉"
-    title = "Increase heading level"
-
-    def execute(self, *args, **kwargs):
-        content = self.get_text_textarea("Markdown content", "Input Markdown content")
-        new_content = increase_heading_level_content(content)
-        self.add_line(new_content)
-        clipboard = QApplication.clipboard()
-        clipboard.setText(new_content, QClipboard.Clipboard)
-        QMessageBox.information(None, "Copy", "Text copied to clipboard!")
-
-
-import yaml, re
-
-def identify_code_blocks(lines):
-    code_block_delimiter = None
-    for line in lines:
-        match = re.match(r"^(`{3,})(.*)", line)
-        if match:
-            delimiter = match.group(1)
-            if code_block_delimiter is None:
-                code_block_delimiter = delimiter
-            elif code_block_delimiter == delimiter:
-                code_block_delimiter = None
-            yield line, True
-            continue
-        if code_block_delimiter:
-            yield line, True
-        else:
-            yield line, False
-
-def increase_heading_level_content(markdown_text: str) -> str:
-    parts = markdown_text.split("---", 2)
-    if len(parts) < 3:
-        yaml_md, content_md = "", markdown_text
-    else:
-        yaml_md, content_md = f"---{parts[1]}---", parts[2].lstrip()
-
-    new_lines = []
-    lines = content_md.split("\n")
-    for line, is_code_block in identify_code_blocks(lines):
-        if is_code_block:
-            new_lines.append(line)
-            continue
-
-        new_lines.append("#" + line if line.startswith('#') else line)
-    content_md = "\n".join(new_lines)
-
-    return yaml_md + "\n\n" + content_md
