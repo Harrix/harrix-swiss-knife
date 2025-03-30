58ddc5e7ac1f3838e99da962c9d547296b515917
Sergienko Anton <sergienkoanton@mail.ru>
2025-03-30 09:47:04 +0300

    ♻️ Refactor class OnDownloadAndReplaceImagesFolder

--- src/harrix_swiss_knife/actions_md.py
+++ src/harrix_swiss_knife/actions_md.py
@@ -65,19 +65,26 @@ class OnDownloadAndReplaceImages(action_base.ActionBase):
         self.show_result()
 
 
-class OnDownloadAndReplaceImagesFolder(action_base.ActionBase): # ⚠️ TODO
+class OnDownloadAndReplaceImagesFolder(action_base.ActionBase):
     icon = "📥"
     title = "Download images in …"
 
     def execute(self, *args, **kwargs):
-        folder_path = self.get_existing_directory("Select a folder with Markdown files", config["path_articles"])
-        if not folder_path:
+        self.folder_path = self.get_existing_directory("Select a folder with Markdown files", config["path_articles"])
+        if not self.folder_path:
             return
 
+        self.start_thread(self.in_thread, self.thread_after, self.title)
+
+    def in_thread(self):
         try:
-            self.add_line(h.file.apply_func(folder_path, ".md", h.md.download_and_replace_images))
+            self.add_line(h.file.apply_func(self.folder_path, ".md", h.md.download_and_replace_images))
         except Exception as e:
             self.add_line(f"❌ Error: {e}")
+
+    def thread_after(self, result):
+        self.show_toast(f"{self.title} {self.folder_path} completed")
+        self.show_result()
 
 
 class OnFormatYaml(action_base.ActionBase): # ⚠️ TODO
