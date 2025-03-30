218a3bf10df3bb6e5b868b4e2c4b909ab129f6e7
Sergienko Anton <sergienkoanton@mail.ru>
2025-03-30 10:56:04 +0300

    ♻️ Refactor class OnGenerateImageCaptionsFolder

--- src/harrix_swiss_knife/actions_md.py
+++ src/harrix_swiss_knife/actions_md.py
@@ -156,19 +156,26 @@ class OnGenerateImageCaptions(action_base.ActionBase):
         self.show_result()
 
 
-class OnGenerateImageCaptionsFolder(action_base.ActionBase): # ⚠️ TODO
+class OnGenerateImageCaptionsFolder(action_base.ActionBase): 
     icon = "🌄"
     title = "Add image captions in …"
 
     def execute(self, *args, **kwargs):
-        folder_path = self.get_existing_directory("Select a folder with Markdown files", config["path_articles"])
-        if not folder_path:
+        self.folder_path = self.get_existing_directory("Select a folder with Markdown files", config["path_articles"])
+        if not self.folder_path:
             return
 
+        self.start_thread(self.in_thread, self.thread_after, self.title)
+
+    def in_thread(self):
         try:
-            self.add_line(h.file.apply_func(folder_path, ".md", h.md.generate_image_captions))
+            self.add_line(h.file.apply_func(self.folder_path, ".md", h.md.generate_image_captions))
         except Exception as e:
             self.add_line(f"❌ Error: {e}")
+
+    def thread_after(self, result):
+        self.show_toast(f"{self.title} {self.folder_path} completed")
+        self.show_result()
 
 
 class OnGenerateToc(action_base.ActionBase): # ⚠️ TODO
