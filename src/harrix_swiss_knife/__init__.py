6c7407aab21858f2f5d53c2508f7b8f7cec94996
Sergienko Anton <sergienkoanton@mail.ru>
2025-05-12 10:57:36 +0300

    🐞 Fix __init__.py

--- src/harrix_swiss_knife/__init__.py
+++ src/harrix_swiss_knife/__init__.py
@@ -1,6 +1,10 @@
-from harrix_swiss_knife import actions_apps as apps  # noqa
-from harrix_swiss_knife import actions_dev as dev  # noqa
-from harrix_swiss_knife import actions_file as file  # noqa
-from harrix_swiss_knife import actions_images as images  # noqa
-from harrix_swiss_knife import actions_md as md  # noqa
-from harrix_swiss_knife import actions_py as py  # noqa
+"""harrix-swiss-knife - Python + Node.js application for automating personal tasks in Windows."""
+
+from harrix_swiss_knife import actions_apps as apps
+from harrix_swiss_knife import actions_dev as dev
+from harrix_swiss_knife import actions_file as file
+from harrix_swiss_knife import actions_images as images
+from harrix_swiss_knife import actions_md as md
+from harrix_swiss_knife import actions_py as py
+
+__all__ = ["apps", "dev", "file", "images", "md", "py"]
