94cf7a07bb8aaad4c49e248367748bf04ddf1d0b
Sergienko Anton <sergienkoanton@mail.ru>
2025-07-08 13:43:54 +0300

    🐞 Fix ruff check issues

--- src/harrix_swiss_knife/__init__.py
+++ src/harrix_swiss_knife/__init__.py
@@ -1,10 +1,10 @@
 """harrix-swiss-knife - Python + Node.js application for automating personal tasks in Windows."""
 
 from harrix_swiss_knife import fitness, main_menu_base, tray_icon
-from harrix_swiss_knife.actions import apps
+from harrix_swiss_knife.actions import apps as apps
 from harrix_swiss_knife.actions import development as dev
 from harrix_swiss_knife.actions import files as file
-from harrix_swiss_knife.actions import images
+from harrix_swiss_knife.actions import images as images
 from harrix_swiss_knife.actions import markdown as md
 from harrix_swiss_knife.actions import python as py
 from harrix_swiss_knife.actions.base import ActionBase
