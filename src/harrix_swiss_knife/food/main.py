65466bdce493de201c2045a441e8f5d19b90ccbf
Sergienko Anton <sergienkoanton@mail.ru>
2025-07-20 22:38:13 +0300

    🔧 Modify food: Enable Enter key activation for pushButton_food_add

--- src/harrix_swiss_knife/food/main.py
+++ src/harrix_swiss_knife/food/main.py
@@ -282,6 +282,7 @@ class MainWindow(
                 focused_widget == self.doubleSpinBox_food_calories
                 or focused_widget == self.spinBox_food_weight
                 or focused_widget == self.checkBox_food_is_drink
+                or focused_widget == self.pushButton_food_add
             ):
                 self.pushButton_food_add.click()
                 return
