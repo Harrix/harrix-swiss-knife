c734ec02305c006340e407e97d0dac6be6fe47c9
Sergienko Anton <sergienkoanton@mail.ru>
2026-01-05 21:37:19 +0300

    ➕ Add UI elements for habbits tracking in fitness app

--- src/harrix_swiss_knife/apps/fitness/recover.sql
+++ src/harrix_swiss_knife/apps/fitness/recover.sql
@@ -2,7 +2,7 @@ CREATE TABLE "exercises" (
 	"_id"	INTEGER,
 	"name"	TEXT NOT NULL,
 	"unit"	TEXT,
-	"is_type_required"	INTEGER NOT NULL DEFAULT 0, 
+	"is_type_required"	INTEGER NOT NULL DEFAULT 0,
 	calories_per_unit REAL DEFAULT 0,
 	PRIMARY KEY("_id" AUTOINCREMENT)
 );
@@ -149,3 +149,18 @@ INSERT INTO "types" ("_id", "_id_exercises", "type", "calories_modifier") VALUES
 (73, 10, '6.5 kg', 1.0),
 (74, 34, 'In park', 1.1),
 (75, 9, '6.5 kg in each hand', 1.0);
+
+CREATE TABLE "habbits" (
+	"_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"is_bool"	INTEGER,
+	PRIMARY KEY("_id" AUTOINCREMENT)
+)
+
+CREATE TABLE "process_habbits" (
+	"_id"	INTEGER NOT NULL,
+	"_id_habbit"	INTEGER NOT NULL,
+	"value"	INTEGER NOT NULL,
+	"date"	TEXT NOT NULL,
+	PRIMARY KEY("_id" AUTOINCREMENT)
+)
