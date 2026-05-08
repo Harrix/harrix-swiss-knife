58242bb5a20332eb93f8f5a9d33d6ef8cff4a405
Sergienko Anton <sergienkoanton@mail.ru>
2026-05-08 08:39:46 +0300

    🗑️ Remove install-all-offline.bat and install.bat

--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Convenience entrypoint for Windows users.
-REM Delegates to install\install.bat (which runs the PowerShell deploy elevated).
-
-cd /d "%~dp0"
-call ".\install\install.bat"
