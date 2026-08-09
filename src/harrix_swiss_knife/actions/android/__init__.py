9b73c7c4fe4e406bf83b84b231a2291c01e8288f
Sergienko Anton <sergienkoanton@mail.ru>
2026-08-09 10:08:23 +0300

    🐞 Fix CLI startup failing when qrcode missing from uv tool env

--- src/harrix_swiss_knife/actions/android/__init__.py
+++ src/harrix_swiss_knife/actions/android/__init__.py
@@ -3,11 +3,9 @@
 from harrix_swiss_knife.actions.android.android_build import OnAndroidBuild
 from harrix_swiss_knife.actions.android.android_check import OnAndroidCheck
 from harrix_swiss_knife.actions.android.android_format import OnAndroidFormat
-from harrix_swiss_knife.actions.android.photo_sync import OnPhotoSync
 
 __all__ = [
     "OnAndroidBuild",
     "OnAndroidCheck",
     "OnAndroidFormat",
-    "OnPhotoSync",
 ]
